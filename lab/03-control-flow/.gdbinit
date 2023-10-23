40872f55ed4592c3b62cc53fc251d99d6d46879b
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-23 17:15:38 +0800

    add: 添加控制流

---
+++ lab/03-control-flow/.gdbinit
@@ -0,0 +1,1 @@
+set disassemble-next-line on
---
+++ lab/03-control-flow/adder.c
@@ -0,0 +1,14 @@
+int a, b, c;
+
+int main(int argc, char *argv[])
+{
+	a = 3;
+	b = 4;
+	c = a + b;
+
+	if (c > 0) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
