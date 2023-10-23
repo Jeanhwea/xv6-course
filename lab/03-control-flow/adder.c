a895f3a584e2f4b8a084c5daa8d08f918bec08a3
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-23 20:22:53 +0800

    update: adder

--- lab/03-control-flow/adder.c
+++ lab/03-control-flow/adder.c
@@ -1,14 +1,13 @@
-int a, b, c;
+int add(int a, int b) {
+	int c = a + b;
+	return c;
+}
 
 int main(int argc, char *argv[])
 {
+	int a, b;
 	a = 3;
 	b = 4;
-	c = a + b;
-
-	if (c > 0) {
-		return 0;
-	} else {
-		return 1;
-	}
+	c = add(a, b);
+	return c;
 }
