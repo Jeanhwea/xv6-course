b1789e3083583a6cb87487528b0393c6a4362daa
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-25 10:08:49 +0800

    update: 添加代码逻辑

---
+++ lab/04-qemu/05-qemu-cdrom-install.sh
@@ -0,0 +1,18 @@
+cd ~/vm/disks
+
+if [ ! -f minix_R3.3.0-588a35b.iso ]; then
+   cat << EOF
+Please download images first
+
+   wget -c https://jaist.dl.sourceforge.net/project/archiveos/m/minix/minix_R3.3.0-588a35b.iso.bz2
+   bzip2 -d minix_R3.3.0-588a35b.iso.bz2
+
+EOF
+fi
+
+
+qemu-system-x86_64 \
+    -smp 4 -m 4G \
+    -enable-kvm \
+    -hda hd001.img \
+    -cdrom minix_R3.3.0-588a35b.iso
