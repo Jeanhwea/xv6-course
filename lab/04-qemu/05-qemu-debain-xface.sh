134565b749b9f171d6a181b4f45a11fe25c978a1
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-25 09:21:42 +0800

    add: qemu debian xface desktop

---
+++ lab/04-qemu/05-qemu-debain-xface.sh
@@ -0,0 +1,9 @@
+cd ~/vm/disks
+
+[ -f debian-live-12.2.0-amd64-xfce.iso ] || \
+    wget -c https://mirrors.tuna.tsinghua.edu.cn/debian-cd/current-live/amd64/iso-hybrid/debian-live-12.2.0-amd64-xfce.iso
+
+
+qemu-system-x86_64 \
+    -smp 4 -m 4G \
+    -hda debian_hd.img -cdrom debian-live-12.2.0-amd64-xfce.iso
