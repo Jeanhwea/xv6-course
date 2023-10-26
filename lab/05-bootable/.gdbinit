3014e7f91b9a9112faf2b6201b4259bc1b236787
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-26 14:01:54 +0800

    update: 添加代码文件

--- lab/05-bootable/.gdbinit
+++ lab/05-bootable/.gdbinit
@@ -1,4 +1,6 @@
+set architecture i8086
+target remote :1234
+
 set disassemble-next-line on
 lay asm
 lay reg
-starti
--- lab/05-bootable/seg01.c
+++
@@ -1,9 +0,0 @@
-int vec[16];
-
-int main (int argc, char *argv[])
-{
-	for (int i = 0; i < 8; ++i) {
-		vec[i] = i;
-	}
-	return vec[3];
-}
