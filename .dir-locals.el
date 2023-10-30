287a8d18e613322f9ab4055a7b4182ee7810286a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-10-30 10:45:20 +0800

    update: locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((nil . ((eval . (add-hook 'before-save-hook #'whitespace-cleanup)))))
+((c-mode .
+   ((eval . (eglot-ensure))
+     (eval . (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
+  (c++-mode .
+    ((eval . (eglot-ensure))
+      (eval . (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
+  (nil . ((eval . (add-hook 'before-save-hook #'whitespace-cleanup)))))
