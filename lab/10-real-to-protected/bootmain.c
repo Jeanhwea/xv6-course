b3884d7c481de46f870e30af6406ebb862ee7f61
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-01 14:10:53 +0800

    update: 添加代码逻辑

--- lab/10-real-to-protected/bootmain.c
+++ lab/10-real-to-protected/bootmain.c
@@ -1,9 +1,10 @@
 #define VGA_ADDR ((char *)0xb8000)
-#define VGA_CTRL_REG 0x3d4
-#define VGA_DATA_REG 0x3d5
+#define CTRL_REG 0x3d4
+#define DATA_REG 0x3d5
 #define MAX_ROWS 25
 #define MAX_COLS 80
-#define GREEN_ON_BLACK 0x0a;
+#define RED_ON_BLACK 0x0c
+#define GREEN_ON_BLACK 0x0a
 
 unsigned char port_in(unsigned short port)
 {
@@ -19,10 +20,10 @@ void port_out(unsigned short port, unsigned char data)
 
 int get_cursor()
 {
-	port_out(VGA_CTRL_REG, 14);
-	unsigned char high = port_in(VGA_DATA_REG);
-	port_out(VGA_CTRL_REG, 15);
-	unsigned char low = port_in(VGA_DATA_REG);
+	port_out(CTRL_REG, 14);
+	unsigned char high = port_in(DATA_REG);
+	port_out(CTRL_REG, 15);
+	unsigned char low = port_in(DATA_REG);
 	int offset = (int)(high << 8) + (int)low;
 	return offset * 2;
 }
@@ -32,10 +33,10 @@ void set_cursor(int offset)
 	offset /= 2;
 	unsigned char high = (unsigned char)(offset >> 8);
 	unsigned char low = (unsigned char)(offset & 0xff);
-	port_out(VGA_CTRL_REG, 14);
-	port_out(VGA_DATA_REG, high);
-	port_out(VGA_CTRL_REG, 15);
-	port_out(VGA_DATA_REG, low);
+	port_out(CTRL_REG, 14);
+	port_out(DATA_REG, high);
+	port_out(CTRL_REG, 15);
+	port_out(DATA_REG, low);
 }
 
 int get_offset(int col, int row)
@@ -45,17 +46,26 @@ int get_offset(int col, int row)
 
 void clear_screen()
 {
-	char *videomem = VGA_ADDR;
+	char *video = VGA_ADDR;
 	for (int col = 0; col < MAX_COLS; ++col) {
 		for (int row = 0; row < MAX_ROWS; ++row) {
-			videomem[get_offset(col, row)] = ' ';
+			video[get_offset(col, row)] = ' ';
 		}
 	}
+}
+
+void print_char(char ch)
+{
+	char *video = VGA_ADDR;
+	int curr = get_cursor();
+	video[curr] = ch;
 }
 
 void start_kernel()
 {
 	char *video = VGA_ADDR;
+
+	// print_char('X');
 
 	char *msg = "Hello World\n";
 	for (char *p = msg; *p != '\n'; ++p) {
