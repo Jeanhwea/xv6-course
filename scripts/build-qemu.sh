41dacfe4c421e32959b3ba9ffddcc69e988082ad
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-02 13:53:57 +0800

    add

---
+++ scripts/build-qemu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+PREFIX=/opt/qemu
+FILEGZ="$HOME/down/bochs-2.7.tar.gz"
+
+sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build \
+     git-email \
+     libaio-dev libbluetooth-dev libcapstone-dev libbrlapi-dev libbz2-dev \
+     libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev \
+     libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev \
+     librbd-dev librdmacm-dev \
+     libsasl2-dev libsdl2-dev libseccomp-dev libsnappy-dev libssh-dev \
+     libvde-dev libvdeplug-dev libvte-2.91-dev libxen-dev liblzo2-dev \
+     valgrind xfslibs-dev \
+     libnfs-dev libiscsi-dev
+
+# TODO
