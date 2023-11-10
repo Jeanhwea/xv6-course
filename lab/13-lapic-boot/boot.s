b1f69457067bb06d79b80b769667d457faddfb09
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-10 16:59:03 +0800

    add

--- lab/13-lapic-boot/boot.s
+++ lab/13-lapic-boot/boot.s
@@ -1,7 +1,15 @@
 [org 0x7c00]
-[bits 16]
 
-start:
+APIC_ID  equ 0xfee00020
+APIC_SVR equ 0xfee000f0
+APIC_ICR equ 0xfee00300
+
+AP_ENTRY equ 0x00008000
+
+
+;; bootstrap processor
+[bits 16]
+s_bsp:
 	xor	ax, ax
 	mov	ds, ax
 	mov	ss, ax
@@ -9,15 +17,14 @@ start:
 	mov	fs, ax
 	mov	gs, ax
 
-	cli
 	lgdt	[desc]
 	mov	eax, cr0
 	or	eax, 0x1
 	mov	cr0, eax
-	jmp	CODE_SEG:start32
+	jmp	CODE_SEG:s32_bsp
 
-
-start32:
+[bits 32]
+s32_bsp:
 	mov	ax, DATA_SEG
 	mov	ds, ax
 	mov	ss, ax
@@ -27,17 +34,98 @@ start32:
 	mov	ebp, 0x90000
 	mov	esp, ebp
 
-	hlt
+	mov	esi, s_ap_entry
+	mov	edi, AP_ENTRY
+	mov	ecx, s_ap_entry - s_ap
+	cld
+	rep movsb
 
+	; enable apic
+	mov	eax, [APIC_SVR]
+	or	eax, 0x00000100	; APIC software enable
+	mov	[APIC_SVR], eax
+
+	; sync other APs
+	mov	eax, 0x000c4500
+	mov	[APIC_ICR], eax
+
+	mov	ecx, 100000000	; sleep for while
+	loop	$
+
+	;; send SIPI to other APs
+	mov	eax, 0x000c4600 | (AP_ENTRY) >> 12
+	mov	[APIC_SVR], eax
+
+	mov	ecx, 100000000	; sleep for while
+	loop	$
+
+	mov	ebx, [APIC_ID]
+	shr	ebx, 24
+
+	mov	edi, 0x000b8a00
+	mov	eax, ebx
+	mov	cl, 10
+	div	cl
+	add	ah, '0'
+	mov	[edi+2*ebx], ah	; print local_apic_id % 10
+	mov	byte [edi+2*ebx+1], 0x1f
+
+	hlt
+	jmp	$
+
+
+;; application processor
+[bits 16]
+s_ap:
+	jmp	0x0000:s_ap_entry
+
+s_ap_entry:
+	xor	ax, ax
+	mov	ds, ax
+	mov	ss, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	gs, ax
+
+	lgdt	[desc]
+	mov	eax, cr0
+	or	eax, 0x1
+	mov	cr0, eax
+	jmp	CODE_SEG:s32_ap
+
+[bits 32]
+s32_ap:
+	mov	ax, DATA_SEG
+	mov	ds, ax
+	mov	ss, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	gs, ax
+	mov	ebp, 0x90000
+	mov	esp, ebp
+
+	mov	ebx, [APIC_ID]
+	shr	ebx, 24
+
+	mov	edi, 0x000b8a00
+	mov	eax, ebx
+	mov	cl, 10
+	div	cl
+	add	ah, '0'
+	mov	[edi+2*ebx], ah	; print local_apic_id % 10
+	mov	byte [edi+2*ebx+1], 0x1f
+
+	hlt
+	jmp	$
 
 gdt_begin:
     dd 0, 0
 
 gdt_code:
-    dd 0x0000ffff, 0x00cf9b00
+    dd 0x0000ffff, 0x00cf9a00
 
 gdt_data:
-    dd 0x0000ffff, 0x00cf9300
+    dd 0x0000ffff, 0x00cf9200
 
 gdt_end:
 
