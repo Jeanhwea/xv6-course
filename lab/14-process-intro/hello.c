64b80d93a06d3d968fb54054bf6f4b68b3914ccf
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-13 16:04:09 +0800

    update: 添加代码

---
+++ lab/14-process-intro/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (1) {
+		i++;
+		// printf("pid = %d\n", getpid());
+		// sleep(1);
+	}
+	return 0;
+}
