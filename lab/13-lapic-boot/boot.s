e8218bf69ce2cb04ff6ea39e73ce6e8ac18f6dff
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-13 11:20:16 +0800

    update

--- lab/13-lapic-boot/boot.s
+++ lab/13-lapic-boot/boot.s
@@ -1,12 +1,12 @@
 [org 0x7c00]
 
-APIC_ID  equ 0xfee00020
-APIC_SVR equ 0xfee000f0
-APIC_ICR equ 0xfee00300
+APIC_ID  equ 0xfee00020		; local apic id register
+APIC_SVR equ 0xfee000f0		; spurious interrupt vector register
+APIC_ICR equ 0xfee00300		; interrupt command register
 
-VGA      equ 0x000b8a00
-AP_ENTRY equ 0x00008000
-PT_STACK equ 0x90000
+VGA      equ 0x000b8a00		; vga address
+AP_ENTRY equ 0x00008000		; AP entry
+PT_STACK equ 0x90000		; stack pointer
 
 
 ;; BSP: Bootstrap Processor
@@ -55,20 +55,20 @@ s32_bsp:
 
 	;; Send SIPI to other APs
 	mov	eax, 0x000c4600 | (AP_ENTRY) >> 12
-	mov	[APIC_SVR], eax
+	mov	[APIC_ICR], eax
 
 	;; Read APIC ID
 	mov	ebx, [APIC_ID]	; wait for write finish, by reading
 	shr	ebx, 24
 
 	;; Print Local APIC ID LSB digital
-	mov	edi, VGA
-	mov	eax, ebx
-	mov	cl, 10
-	div	cl
-	add	ah, '0'
-	mov	[edi+2*ebx], ah
-	mov	byte [edi+2*ebx+1], 0x1f
+	; mov	edi, VGA
+	; mov	eax, ebx
+	; mov	cl, 10
+	; div	cl
+	; add	ah, '0'
+	; mov	[edi+2*ebx], ah
+	; mov	byte [edi+2*ebx+1], 0x1f
 
 	hlt
 
