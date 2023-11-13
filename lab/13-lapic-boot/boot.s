2d024d548db98c6529b5234c8251c79d51db6932
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-13 14:03:07 +0800

    update: 添加代码

--- lab/13-lapic-boot/boot.s
+++ lab/13-lapic-boot/boot.s
@@ -1,97 +1,73 @@
 [org 0x7c00]
 
 APIC_ID  equ 0xfee00020		; local apic id register
-APIC_SVR equ 0xfee00000		; spurious interrupt vector register
+APIC_SVR equ 0xfee000f0		; spurious interrupt vector register
 APIC_ICR equ 0xfee00300		; interrupt command register
 
-VGA      equ 0x000b8a00		; vga address
-AP_ENTRY equ 0x8000		; AP entry
-PT_STACK equ 0x10000		; stack pointer
+VGA         equ 0x000b8a00	; vga address
+PT_AP_ENTRY equ 0x8000		; AP entry address
+PT_STACK    equ 0x9000		; stack pointer
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BSP: Bootstrap Processor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 [bits 16]
-s_bsp:
+bsp_start:
+	cli
 	xor	ax, ax
 	mov	ds, ax
-	mov	ss, ax
-	mov	es, ax
-	mov	ebp, PT_STACK
-	mov	esp, ebp
-
 	lgdt	[desc]
 	mov	eax, cr0
 	or	eax, 0x1
 	mov	cr0, eax
-	jmp	CODE_SEG:s32_bsp
+	jmp	CODE_SR:bsp_start32
 
 [bits 32]
-s32_bsp:
-	;; Init Core
-	mov	ax, DATA_SEG
+bsp_start32:
+	mov	ax, DATA_SR
 	mov	ds, ax
-	mov	ss, ax
-	mov	es, ax
 	mov	ebp, PT_STACK
 	mov	esp, ebp
 
-	;; Load AP's jump text
-	mov	esi, s_ap_entry
-	mov	edi, AP_ENTRY
-	mov	ecx, s_ap_entry - s_ap
+	;; Load AP's Jump Code to PT_AP_ENTRY
+	mov	esi, ap_start
+	mov	edi, PT_AP_ENTRY
+	mov	ecx, ap_entry - ap_start
 	cld
 	rep movsb
-
-	; Set up the APIC base address in MSR
-	mov	ecx, 0x1b
-	rdmsr
-	or	eax, 1 << 11   ; Set the ENABLE bit
-	wrmsr
 
 	;; Enable APIC
 	mov	eax, [APIC_SVR]
 	or	eax, (1 << 8)	; APIC software enable
 	mov	[APIC_SVR], eax
 
-	mov	eax, [APIC_ID]	; wait for write finish, by reading
+	; mov	eax, [APIC_ID]	; barrier
 
 	;; Sync other APs
 	mov	eax, 0x000c4500
 	mov	[APIC_ICR], eax
-
-	mov	eax, [APIC_ID]	; wait for write finish, by reading
+	; mov	eax, [APIC_ID]	; barrier
 
 	;; Send SIPI to other APs
-	mov	eax, 0x000c4600 | (AP_ENTRY >> 12)
+	mov	eax, 0x000c4600 | (PT_AP_ENTRY >> 12)
 	mov	[APIC_ICR], eax
 
 	;; Read APIC ID
 	mov	ebx, [APIC_ID]	; wait for write finish, by reading
 	shr	ebx, 24
+	call	print_lsb_digit
 
-	;; Print Local APIC ID LSB digital
-	mov	edi, VGA
-	mov	eax, ebx
-	mov	cl, 10
-	div	cl
-	add	ah, '0'
-	mov	[edi+2*ebx], ah
-	mov	byte [edi+2*ebx+1], 0x2f
+	hlt
 
-
-spin:
-	;; Send SIPI to other APs
-	mov	eax, 0x000c4600 | (AP_ENTRY >> 12)
-	mov	[APIC_ICR], eax
-	jmp	spin
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; AP: Application Processor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 [bits 16]
-s_ap:
-	jmp	0x0000:s_ap_entry
+ap_start:
+	jmp	0x0000:ap_entry
 
-s_ap_entry:
+ap_entry:
+	cli
 	xor	ax, ax
 	mov	ds, ax
 	mov	ss, ax
@@ -101,32 +77,35 @@ s_ap_entry:
 	mov	eax, cr0
 	or	eax, 0x1
 	mov	cr0, eax
-	jmp	CODE_SEG:s32_ap
+	jmp	CODE_SR:s32_ap
 
 [bits 32]
 s32_ap:
 	;; Init Core
-	mov	ax, DATA_SEG
+	mov	ax, DATA_SR
 	mov	ds, ax
-	mov	ss, ax
-	mov	es, ax
 	mov	ebp, PT_STACK
 	mov	esp, ebp
 
 	;; Read APIC ID
 	mov	ebx, [APIC_ID]
 	shr	ebx, 24
+	call	print_lsb_digit
 
-	; Print Local APIC ID LSB digital
+	hlt
+
+;; print lsb digit in ebx
+print_lsb_digit:
+	pusha
 	mov	edi, VGA
 	mov	eax, ebx
 	mov	cl, 10
 	div	cl
 	add	ah, '0'
 	mov	[edi+2*ebx], ah
-	mov	byte [edi+2*ebx+1], 0x1f
-
-	hlt
+	mov	byte [edi+2*ebx+1], 0xcf
+	popa
+	ret
 
 ;; setup gdt
 gdt_begin:
@@ -146,8 +125,8 @@ desc:
 	dw gdt_end - gdt_begin - 1
 	dd gdt_begin
 
-CODE_SEG equ gdt_code - gdt_begin
-DATA_SEG equ gdt_data - gdt_begin
+CODE_SR equ gdt_code - gdt_begin
+DATA_SR equ gdt_data - gdt_begin
 
 times 510-($-$$) db 0
 	dw 0xaa55
