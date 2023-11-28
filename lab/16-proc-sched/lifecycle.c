bb7a5e7e5901cf991e73b5a8a9f405fb003f6975
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-28 21:37:48 +0800

    add

---
+++ lab/16-proc-sched/lifecycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int pid = fork();
+	if (pid < 0) {
+		printf("fork() failed\n");
+		return 0;
+	}
+
+	if (pid == 0) {
+		char *argv[] = { "ls", "-l" };
+		char *envs[] = { NULL };
+		execve("/bin/ls", argv, envs);
+		exit(0);
+	} else {
+		wait(NULL);
+	}
+	return 0;
+}
